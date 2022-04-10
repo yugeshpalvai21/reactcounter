cf305f6602724634b6371fe706e74b99519c4536
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-10 20:09:36 +0530

    Updates Counter functional component

---
+++ src/Counter.js
@@ -0,0 +1,9 @@
+const Counter = () => {
+  return (
+    <div>
+      <h2>Welcome To Counter App</h2>
+    </div>
+  )
+}
+
+export default Counter;
--- src/Couter.js
+++
@@ -1,9 +0,0 @@
-function Counter() {
-  return (
-    <div className="App">
-      <p>Welcome To Counter App</p>
-    </div>
-  );
-}
-
-export default Counter;
