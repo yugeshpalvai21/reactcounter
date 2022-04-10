57dee467315d30abff0b7b90a3353a1981fe03c4
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-10 20:12:38 +0530

    sets counter with useState state

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
       <h2>Welcome To Counter App</h2>
+      <h4>Counter Value - { counter }</h4>
     </div>
   )
 }
