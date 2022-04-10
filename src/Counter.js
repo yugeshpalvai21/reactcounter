14c7d4ea8ab9f181273948a715389e6b061004ea
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-10 20:30:54 +0530

    implements second solution to calling functions when onclick events happens

--- src/Counter.js
+++ src/Counter.js
@@ -3,13 +3,18 @@ import { useState } from 'react';
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  const handleReset = () => {
+    console.log('Resetting Counter To Zero');
+    setCounter(0);
+  }
+
   return (
     <div>
       <h2>Welcome To Counter App</h2>
       <h4>Counter Value - { counter }</h4>
       <button onClick={() => (setCounter(counter+1)) }>Add One</button>
       <button onClick={() => (setCounter(counter-1)) }>Minus One</button>
-      <button onClick={() => (setCounter(0)) }>Reset Counter</button>
+      <button onClick={handleReset}>Reset Counter</button>
     </div>
   )
 }
