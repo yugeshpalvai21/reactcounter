18711df6df82b5dbcfbebce3abc542695258eeee
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-10 20:20:00 +0530

    adding onclick events to buttons

--- src/Counter.js
+++ src/Counter.js
@@ -7,6 +7,9 @@ const Counter = () => {
     <div>
       <h2>Welcome To Counter App</h2>
       <h4>Counter Value - { counter }</h4>
+      <button onClick={() => {console.log('adding one')}}>Add One</button>
+      <button onClick={() => {console.log('minus one')}}>Minus One</button>
+      <button onClick={() => {console.log('resetting counter')}}>Reset Counter</button>
     </div>
   )
 }
